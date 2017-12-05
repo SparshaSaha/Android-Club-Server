c3b741e52c7e60187ad10d122b003c53664bd51d
Sparsha Saha <saha.sparsha@gmail.com>
2017-12-06 00:02:12 +0530

    string length issue changed

--- Routes.js
+++ Routes.js
@@ -4,7 +4,7 @@ const User=require("./Models/User");
 module.exports= function(app,mongo){
   app.get("/upload_details",(req,res)=>{
 
-    var user={
+    var user=new User{
 
       reg_no:req.query.reg_no,
       mac:req.query.mac,
@@ -18,6 +18,18 @@ module.exports= function(app,mongo){
         c=c+user.reg_no;
         t=t+String.fromCharCode(c%127);
       }
+
+      user.unique_msg=t;
+
+      user.save((err,res1)=>
+    {
+      if(err)
+      res.send("Error....Upload not sucessful");
+      else {
+        res.send(user.unique_msg);
+      }
+
+    });
 
       res.send(t);
   });
