0a513c7d16e969e5cc1ebad71e6109ac1dfc2aca
Sparsha Saha <saha.sparsha@gmail.com>
2017-12-05 23:46:43 +0530

    unique code generator added

--- Routes.js
+++ Routes.js
@@ -10,8 +10,15 @@ module.exports= function(app,mongo){
       mac:req.query.mac,
       unique_msg:""
     }
-
+    var t=""
     //calculate unique message
-    
+      for(int i=0;i<user.mac.length();i++)
+      {
+        var c=user.mac.charAt(i).charCodeAt(0);
+        c=c+user.reg_no;
+        t=t+String.fromCharCode(c%127);
+      }
+
+      res.send(t);
   });
 }
