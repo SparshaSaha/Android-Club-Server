9d9fd06b60f096d14b0cb3c58958a739ac8afec6
Sparsha Saha <saha.sparsha@gmail.com>
2017-12-06 00:10:06 +0530

    Few issues resolved

--- Routes.js
+++ Routes.js
@@ -4,12 +4,12 @@ const User=require("./Models/User");
 module.exports= function(app,mongo){
   app.get("/upload_details",(req,res)=>{
 
-    var user=new User{
+    var user=new User({
 
       reg_no:req.query.reg_no,
       mac:req.query.mac,
       unique_msg:""
-    }
+    });
     var t=""
     //calculate unique message
       for(var i=0;i<=user.mac.length;i++)
